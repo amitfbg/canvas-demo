e2612dbf97f9d84f5fe0b8ad564a08faba984753
Amit Kumar <amitfbg570990@gmail.com>
2021-01-25 16:31:27 +0530

    icon

--- src/components/maincontainer/components/PageHeader.js
+++ src/components/maincontainer/components/PageHeader.js
@@ -1,7 +1,16 @@
 import React from "react";
+import RocketIcon from "../../../assets/RocketIcon";
+import Typography from "@material-ui/core/Typography";
 
 function PageHeader() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <RocketIcon />
+      <Typography variant="h3" color="initial">
+        RELEASE
+      </Typography>
+    </div>
+  );
 }
 
 export default PageHeader;
