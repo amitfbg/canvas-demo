94e02f4f917b8483251002f596aa88d8d6aa8ec3
Amit Kumar <amitfbg570990@gmail.com>
2021-01-25 15:15:28 +0530

    breadCrumb completed

---
+++ src/components/breadCrumb/BreadCrumb.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Breadcrumbs, makeStyles } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+
+const useStyles = makeStyles({
+  breadCrumb: {
+    padding: "0.5rem 0 0.5rem 2rem",
+    // color: "#80868b",
+  },
+});
+
+function BreadCrumb() {
+  const classes = useStyles();
+  return (
+    <div className={classes.breadCrumb}>
+      <Breadcrumbs
+        separator={<NavigateNextIcon fontSize="small" />}
+        aria-label="breadcrumb"
+      >
+        <Typography color="textPrimary">Overview</Typography>
+        <Typography color="textSecondary">Release</Typography>
+      </Breadcrumbs>
+    </div>
+  );
+}
+
+export default BreadCrumb;
