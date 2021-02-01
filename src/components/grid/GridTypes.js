9402c839824de5a55aadc85ff57138e45e88a256
Amit Kumar <amitfbg570990@gmail.com>
2021-02-01 12:09:01 +0530

    GridTypes component created

---
+++ src/components/grid/GridTypes.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Grid, makeStyles, Paper } from "@material-ui/core";
+
+const useStyles = makeStyles((theme, props) => ({
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    height: (props) => props.height,
+    borderRadius: "10px",
+    border: "solid 1px #dddddd",
+  },
+}));
+
+export const SmallGrid = (props) => {
+  const classes = useStyles(props);
+  return (
+    <Grid item xs={12} sm={6} lg={3}>
+      <Paper className={classes.paper}>{props.name}</Paper>
+    </Grid>
+  );
+};
+
+export const MediumGrid = (props) => {
+  const classes = useStyles(props);
+  return (
+    <Grid item xs={12} sm={12} lg={6}>
+      <Paper className={classes.paper}>{props.name}</Paper>
+    </Grid>
+  );
+};
--- src/components/maincontainer/MainContainer.js
+++ src/components/maincontainer/MainContainer.js
@@ -2,6 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import { MediumGrid, SmallGrid } from "../grid/GridTypes";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,33 +16,6 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(3),
     marginRight: theme.spacing(3),
   },
-  paper1: {
-    // flexGrow: 1,
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-    height: "6rem",
-    borderRadius: "10px",
-    border: "solid 1px #dddddd",
-  },
-  paper2: {
-    // flexGrow: 1,
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-    height: "17rem",
-    borderRadius: "10px",
-    border: "solid 1px #dddddd",
-  },
-  paper3: {
-    // flexGrow: 1,
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-    height: "35rem",
-    borderRadius: "10px",
-    border: "solid 1px #dddddd",
-  },
 }));
 
 export default function MainContainer() {
@@ -51,43 +25,21 @@ export default function MainContainer() {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid container item spacing={3}>
-          <Grid item xs={12} sm={6} lg={3}>
-            <Paper className={classes.paper1}>Total Projects/MBs</Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <Paper className={classes.paper1}>Impacted Apps</Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <Paper className={classes.paper1}>Pre-deployments</Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <Paper className={classes.paper1}>Total Outages</Paper>
-          </Grid>
+          <SmallGrid name="Total Projects/MBs" height="6rem" />
+          <SmallGrid name="Impacted Apps" height="6rem" />
+          <SmallGrid name="Pre-deployments" height="6rem" />
+          <SmallGrid name="Total Outages" height="6rem" />
         </Grid>
         <Grid container item spacing={3}>
-          <Grid item xs={12} sm={6} lg={3}>
-            <Paper className={classes.paper2}>Project</Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <Paper className={classes.paper2}>Dependency</Paper>
-          </Grid>
-          <Grid item xs={12} sm={12} lg={6}>
-            <Paper className={classes.paper2}>Calender</Paper>
-          </Grid>
+          <SmallGrid name="Projec" height="17rem" />
+          <SmallGrid name="Dependency" height="17rem" />
+          <MediumGrid name="Calender" height="17rem" />
         </Grid>
         <Grid container item spacing={3}>
-          <Grid item xs={12} sm={6} lg={3}>
-            <Paper className={classes.paper3}>Workback</Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <Paper className={classes.paper3}>Leadership</Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <Paper className={classes.paper3}>Release</Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <Paper className={classes.paper3}>Testing</Paper>
-          </Grid>
+          <SmallGrid name="Workback" height="35rem" />
+          <SmallGrid name="Leadership" height="35rem" />
+          <SmallGrid name="Release" height="35rem" />
+          <SmallGrid name="Testing" height="35rem" />
         </Grid>
       </Grid>
     </div>
