3391d087ab33425dd12ec7db1300bc9312cce82e
Aayush Bhalotia <ashubhalotia@gmail.com>
2021-02-01 12:51:11 +0530

    Stepper size issue fixed

--- src/components/CustomStepper/CustomStepper.js
+++ src/components/CustomStepper/CustomStepper.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CustomStepper.css";
 
 const CustomStepper = () => {
   const items = ["PLAN", "INTAKE", "MONITOR"];
+  const [Step, setStep] = useState(0);
+
+  const stepChange = (step) => {
+    console.log(step);
+  };
+
   return (
     <div className="Stepper-container">
       <hr className="starting-line" />
-   <div className="steps">
-   {items.map((step,key) => (
-        <div key={key} className="Stepper-wrapper">
-          <div className="Step-icon"></div>
-          <div  className="Step-label">{step}</div>
-          {key!==items.length-1? <hr className="Step-connector" />:""}
-        </div>
-      ))}
-   </div>
+      <div className="steps">
+        {items.map((step, key) => (
+          <div key={key} className="Stepper-wrapper">
+            <div className="Step-icon" onClick={stepChange(step)}></div>
+            <div className="Step-label">{step}</div>
+            {key !== items.length - 1 ? <hr className="Step-connector" /> : ""}
+          </div>
+        ))}
+      </div>
       <hr className="ending-line" />
     </div>
   );
